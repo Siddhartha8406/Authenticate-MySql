167a9382e2144b6850d6e72a9ec045cb42b51fff
SiddharthaReddy <yerasi.venkat8406@gmail.com>
2022-01-11 00:13:11 +0530

    Added add() and check()

--- temp.py
+++ temp.py
@@ -1,3 +1,33 @@
+#TestDB
+
+import random
 import mysql.connector
 
-db={}
+db=mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'root',
+    database = 'TestDB'
+)
+
+myCursor = db.cursor()
+
+#myCursor.execute("CREATE TABLE Vault(userName varchar(50) NOT NULL, pass varchar(50)NOT NULL, recoveryKey varchar(50))")
+
+def add(uname, password, key):
+    myCursor.execute(f"INSERT INTO Vault(userName, pass, recoveryKey) VALUES('{uname}','{password}','{key}')")
+    db.commit()
+
+#add('Rashmi', 'test1234', 'hdf')
+
+def check(uname):
+    myCursor.execute(f"SELECT * FROM VAULT WHERE userName = '{uname}'")
+    value = False
+    for i in myCursor:
+        print(i)
+        if i != '':
+            value = True
+            break
+    return value
+
+print(check('rashmi'))
